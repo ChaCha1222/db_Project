988d6f5f7d222067e5502bc09a26e040d8ddffa7
ChaCha1222 <ab20031224@gmail.com>
2024-06-10 23:05:59 +0800

    20240610

--- myPurchaseDetail.php
+++ myPurchaseDetail.php
@@ -142,7 +142,7 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="aboutme.php">
                                     <i class="fa fa-user" aria-hidden="true"></i>
-                                    用戶名的個人資訊
+                                    <?php echo $_SESSION['username']; ?>的個人資訊
                                 </a>
                             </li>
                             <li class="nav-item">
